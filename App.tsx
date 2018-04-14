50c70876b4ec7373831fd629901764648ed5e1dd
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-04-14 15:30:09 -0400

    updates

--- App.tsx
+++ App.tsx
@@ -13,9 +13,8 @@ export default class App extends React.Component<Props, State> {
     let { compiler, framework, bundler, styles } = this.props;
     return(
       <div className="content">
-        <h1>Parcel Bundler is freaking awesome</h1>
-        <p>I'm using {compiler} and {framework} with 
-          <a target="_target" href="https://parceljs.org/getting_started.html"> {bundler}</a>
+        <h1>Parcel is freaking awesome</h1>
+        <p>I'm a very simple web application that uses {compiler} and {framework} with <a target="_target" href="https://parceljs.org/getting_started.html">{bundler}</a>
         </p>
         <p>{bundler} is AMAZING. It supports {styles} and {compiler} compilation OUT OF THE BOX</p>
       </div>
